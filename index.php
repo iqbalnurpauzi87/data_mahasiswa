45019a23259adb8078553a38e469977351bf0cb7
iqbalnurpauzi87 <iqbalnurpauzi87@gmail.com>
2023-03-01 07:15:32 +0700

    update programing

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman data mahasiswa</title>
+</head>
+<body>
+    <h2>Data Mahasiswa ELKA UNJ 2018</h2>
+    <a href="" style="padding: 0.4% 0.8%; background-color:#090; color:#fff; border-radius:2px; text-decoration:none;">Tambah data</a>
+<br>
+</br>
+    <table border="1" cellspacing="0" width="50%";>
+    <tr style= "text-align:center;font-weight:bold; background-color:#eee;"> 
+        <td>No</td>
+        <td>NIM</td>
+        <td>Nama mahasiswa</td>
+        <td>Email</td>
+        <td>No Telepon</td>
+        <td>Jurusan</td>
+        <td>Opsi</td>
+    </tr>
+
+<?php
+include 'koneksi.php';
+$no = 1;
+$select = mysqli_query($conn, "SELECT * FROM mahasiswa");
+while($hasil = mysqli_fetch_array($select)){
+?>
+ 
+ <tr style= "text-align:center;"> 
+        <td><?php echo $no++ ?></td>
+        <td><?php echo $hasil['nim'] ?></td>
+        <td><?php echo $hasil['nama_lengkap'] ?></td>
+        <td><?php echo $hasil['email'] ?></td>
+        <td><?php echo $hasil['telepon'] ?></td>
+        <td><?php echo $hasil['jurusan'] ?></td>
+        <td>
+            <a href="">EDIT</a> |
+            <a href="">Hapus</a>
+        </td>
+    </tr>
+    <?php } ?>
+    </table>
+</body>
+</html>
